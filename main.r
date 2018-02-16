828a31c82cb0074ac765286e478e657fd87362c2
Erik Nilsson <eeenilsson@hotmail.com>
2018-02-16 17:09:28 +0100

    minor

--- main.r
+++ main.r
@@ -15,28 +15,26 @@ setwd("c:/e/blog/source")
 ## blogdown::new_site(theme = "MunifTanjim/minimo") ## Minimo
 ## change the working directory to public/, and initialize the GIT repository there:
 ## git remote add origin https://github.com/username/username.github.io
-
-
-## Switch to public/ in shell and clone in shell: https://github.com/eeenilsson/eeenilsson.github.io.git
+## Note: Cannot push to github from work due to firewall
 
 setwd("public")
 
 blogdown::serve_site()
 
 
+## add to config.toml
+## [[menu.main]]
+## name = "Test"
+## weight = -10
+## identifier = "Testing"
+## url = "https://bookdown.org/yihui/blogdown/workflow.html"
 
+## 2. Perhaps you want to add some content. You can add single files
+##    with "hugo new <SECTIONNAME>\<FILENAME>.<FORMAT>".
+## 3. Start the built-in live server via "hugo server".
 
-Just a few more steps and you're ready to go:
-
-1. Download a theme into the same-named folder.
-   Choose a theme from https://themes.gohugo.io/, or
-   create your own with the "hugo new theme <THEMENAME>" command.
-2. Perhaps you want to add some content. You can add single files
-   with "hugo new <SECTIONNAME>\<FILENAME>.<FORMAT>".
-3. Start the built-in live server via "hugo server".
-
-Visit https://gohugo.io/ for quickstart guide and full documentation.
-trying URL 'https://github.com/yihui/hugo-lithium-theme/archive/master.zip'
-Content length 119078 bytes (116 KB)
-downloaded 116 KB
+## Visit https://gohugo.io/ for quickstart guide and full documentation.
+## trying URL 'https://github.com/yihui/hugo-lithium-theme/archive/master.zip'
+## Content length 119078 bytes (116 KB)
+## downloaded 116 KB
 
